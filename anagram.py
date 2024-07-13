ff2e7682c09b05f71c1edcd2286a1c1d9c38d1f0
Copernecium <Copernecium4247@gmail.com>
2024-07-13 20:35:02 +0700

    2

--- anagram.py
+++ anagram.py
@@ -1,20 +1,23 @@
-def is_anagram(a):
-    if len(a) != 2 or type(a) != list:
+def is_anagram(input_here):
+    #step 1 checking input
+    if len(input_here) != 2 or type(input_here) != list:
         return
-    def convert(b):
+    #step 2 remove non alphabet
+    #create funtion can make you don't have to write the same code
+    def convert(checkremove):
         c = ""
-        for i in b:
+        for i in checkremove:
             if i.isalpha() == True:
                 c += i
+        #step 3 convert into set
         return set(c)
-    convert1 = convert(a[0])
-    convert2 = convert(a[1])
-    # print("work",convert1,convert2)
+    convert1 = convert(input_here[0])
+    convert2 = convert(input_here[1])
+    #step 4 checking if it is the same one
     if convert1 == convert2:
         return True
     else:
         return False
-# print(is_anagram(["asda","dsasd"]))
 print(is_anagram(["listen","silent"]))
 print(is_anagram(["television"," it's one evil."]))
 print(is_anagram(["listen", "silentness"]))
