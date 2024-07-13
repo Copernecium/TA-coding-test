d0566fdce698f6d4cb285de6cc92175c722b50f4
Copernecium <Copernecium4247@gmail.com>
2024-07-13 20:45:25 +0700

    3

--- TA.py
+++ TA.py
@@ -1,35 +1,36 @@
-#list all word and price by using tuple
-necessary = {'Noodle_cup':20, 'Fried_egg':24, 'Pad_thai':40, 'Krapao_extra':50, 'water':6}
-not_necessary = {'Chocolate':22, 'Pepsi':18, 'Lemon_tea':15}
-
-#making funtion
+#step 1 making funtion
 def wowza(input_here):
-    #split each food into list
+    
+    #step 2 list all word and price by using tuple
+    necessary = {'Noodle_cup':20, 'Fried_egg':24, 'Pad_thai':40, 'Krapao_extra':50, 'water':6}
+    not_necessary = {'Chocolate':22, 'Pepsi':18, 'Lemon_tea':15}
+    
+    #step 3 split each food into list
     splitword = input_here.split(" ")
     
-    #making summary of all
+    #step 4 making summary of all
     summary_not_ness = 0
     summary_ness = 0
     notincatalog = 0
     
-    #making loop of list
+    #step 5 making loop of list
     for i in splitword:
         
-        #checking type of food
+        #step 6 checking type of food
         if i in not_necessary:
             summary_not_ness+=not_necessary[i]
         elif i in necessary:
             summary_ness+=necessary[i]
         else:
-            #print("there is something not in catalog")
             notincatalog+=1
     
-    #print output
+    #step 7 print output
     print("necessary :",summary_ness,"Baht\n"+"not necessary :",summary_not_ness,"Baht\n"+"total :",summary_ness+summary_not_ness,"Baht")
     if notincatalog != 0:
         print("there is" ,notincatalog, "thing not in catalog")
-    
     print("==========================================================================")
+    
+#step 8 using function to test
 wowza("Chocolate Pepsi Lemon_tea")
 wowza("Noodle_cup Fried_egg Pad_thai Krapao_extra water")
 wowza("Noodle_cup Pepsi Fried_egg water Chocolate Krapao_extra Lemon_tea wowza za")
--- anagram.py
+++ anagram.py
@@ -1,23 +1,30 @@
+#step 1 making function
 def is_anagram(input_here):
-    #step 1 checking input
+    
+    #step 2 checking input
     if len(input_here) != 2 or type(input_here) != list:
         return
-    #step 2 remove non alphabet
+    
+    #step 3 remove non alphabet
     #create funtion can make you don't have to write the same code
     def convert(checkremove):
         c = ""
         for i in checkremove:
             if i.isalpha() == True:
                 c += i
-        #step 3 convert into set
+                
+        #step 4 convert into set
         return set(c)
     convert1 = convert(input_here[0])
     convert2 = convert(input_here[1])
-    #step 4 checking if it is the same one
+    
+    #step 5 checking if it is the same one
     if convert1 == convert2:
         return True
     else:
         return False
+    
+#step 6 using funtion to test
 print(is_anagram(["listen","silent"]))
 print(is_anagram(["television"," it's one evil."]))
 print(is_anagram(["listen", "silentness"]))
