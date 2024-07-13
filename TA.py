922a7b8de207f4e014e2694a3c9ee13c888c4b6a
Copernecium <Copernecium4247@gmail.com>
2024-07-13 21:02:09 +0700

    5

--- TA.py
+++ TA.py
@@ -1,28 +1,28 @@
-#step 1 making funtion
+#step 1 making funtion / สร้าง function
 def wowza(input_here):
     
-    #step 2 checking input
+    #step 2 checking input / เช็คค่าที่รับมาว่าเป็น string ไหม
     if type(input_here) != str:
         print("input is not string\n"
               +"==========================================================================")
         return
     
-    #step 3 list all word and price by using tuple
+    #step 3 list all word and price by using tuple / ลิสรายการทั้งหมด (ที่ใช้ tuple  value มาด้วย)
     necessary = {'Noodle_cup':20, 'Fried_egg':24, 'Pad_thai':40, 'Krapao_extra':50, 'water':6}
     not_necessary = {'Chocolate':22, 'Pepsi':18, 'Lemon_tea':15}
     
-    #step 4 split each food into list
+    #step 4 split each food into list / แยกข้อมูลที่รับมาจากเว้นวรรคแล้วแปลงเป็น list
     splitword = input_here.split(" ")
     
-    #step 5 making summary of all
+    #step 5 making summary of all / สร้างค่าไว้เก็บราคาทุกประเภท
     summary_not_ness = 0
     summary_ness = 0
     notincatalog = 0
     
-    #step 6 making loop of list
+    #step 6 making loop of list / สร้าง loop อิงตามรายการที่เราแยกมา โดยค่าที่รับจะอยู่ใน i
     for i in splitword:
         
-        #step 7 checking type of food
+        #step 7 checking type of food / เช็คประเภทของ i ที่รับมาอยู่ในหมวดไหน แล้วเพิ่มราคาของหมวดนั้น
         if i in not_necessary:
             summary_not_ness+=not_necessary[i]
         elif i in necessary:
@@ -30,13 +30,15 @@ def wowza(input_here):
         else:
             notincatalog+=1
     
-    #step 8 print output
+    #step 8 print output / แสดงผลลัพธ์ออกมา
     print("necessary :",summary_ness,"Baht\n"+"not necessary :",summary_not_ness,"Baht\n"+"total :",summary_ness+summary_not_ness,"Baht")
+    
+    #additional / ถ้าหากมีอันที่อยู่นอกหมวดหมู่ให้แสดงจำนวน
     if notincatalog != 0:
         print("there is" ,notincatalog, "thing not in catalog")
     print("==========================================================================")
     
-#step 9 using function to test
+#step 9 using function to test / ใช้งาน function
 wowza([""])
 wowza("Chocolate Pepsi Lemon_tea")
 wowza("Noodle_cup Fried_egg Pad_thai Krapao_extra water")
--- anagram.py
+++ anagram.py
@@ -1,30 +1,32 @@
-#step 1 making function
+#step 1 making function / สร้าง function ขึ้นมา
 def is_anagram(input_here):
     
-    #step 2 checking input
+    #step 2 checking input if there is not 2 value inside or not lsit / เช็คข้อมูลที่รับมาว่ามีมากกว่าสองตัวหรือไม่ใช่ประเภท list
     if len(input_here) != 2 or type(input_here) != list:
         return
     
-    #step 3 remove non alphabet
-    #create funtion can make you don't have to write the same code
+    #step 3 remove non alphabet / ลบตัวที่ไม่ใช่ตัวอักษรออกไป
+    #create funtion can make you don't have to write the same code / ถ้าทำ function จะทำให้ลดบรรทัดการเขียนได้
     def convert(checkremove):
         c = ""
         for i in checkremove:
             if i.isalpha() == True:
                 c += i
                 
-        #step 4 convert into set
+        #step 4 convert into set / แปลงข้อมูลเป็ฯประเภท set แล้วส่งกลับไป
         return set(c)
+    
+    #step 5 using function we make / ใช้ function ที่สร้างมา
     convert1 = convert(input_here[0])
     convert2 = convert(input_here[1])
     
-    #step 5 checking if it is the same one
+    #step 6 checking if it is the match one / เช็คข้อมูลของ set ทั้งสองตัวว่าตรงกันไหม แล้วส่งค่าเป็น boolean
     if convert1 == convert2:
         return True
     else:
         return False
     
-#step 6 using funtion to test
+#step 7 using funtion to test / ใช้ function เต็มเพื่อ test
 print(is_anagram(["listen","silent"]))
 print(is_anagram(["television"," it's one evil."]))
 print(is_anagram(["listen", "silentness"]))
